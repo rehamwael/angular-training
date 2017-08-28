2de9c08199d27ed300f05302b3676180de977d03
rehamwael <habbasreham@outlook.com>
2017-08-28 21:13:48 +0300

    add crib listing files

---
+++ src/app/add-listing-form/add-listing-form.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit ,ViewChild} from '@angular/core';
+import { CribsService } from './../services/cribs.service';
+import { NgForm } from '@angular/forms';
+@Component({
+  selector: 'app-add-listing-form',
+  templateUrl: './add-listing-form.component.html',
+  styleUrls: ['./add-listing-form.component.css']
+})
+export class AddListingFormComponent implements OnInit {
+
+  @ViewChild('newCribForm') newCribForm: NgForm;	
+  propertyTypes: Array<string> = ['house','kondo','duplex']; 
+
+  constructor(public cribsService: CribsService) { }
+
+  ngOnInit() {
+  }
+
+  onCribSubmit(data): void{
+  	this.cribsService.addCrib(data);
+  	this.newCribForm.reset();
+  }
+
+}
